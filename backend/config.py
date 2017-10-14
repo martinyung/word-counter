246198d099e6feaa3c1fe48fb4c6d7edcea39bee
Yung Chun Ern Martin <martinyce@gmail.com>
2017-10-14 14:54:05 +0800

    add postgress in local environment

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
   DEBUG = False
   SCRF_ENABLED = True
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
   DEBUG = False
